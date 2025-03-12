794ce2e6b9276b7955e0ff21f91986220525c640
Name <vbrfqlj@mail.ru>
2025-03-12 20:54:38 +0300

    Задание 9.3.8

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ class Programm
     delegate bool CheckLengthDelegate(string _row);
     static void Main(string[] args)
     {
-        ShowMessageDelegate showMessageDelegate = ShowMessage;
+        Action showMessageDelegate = ShowMessage;
         showMessageDelegate.Invoke();
 
-        SumDelegate sumDelegate = Sum;
+        Func<int, int, int, int> sumDelegate = Sum;
         int result = sumDelegate.Invoke(1, 30, 120);
         Console.WriteLine(result);
 
-        CheckLengthDelegate checkLengthDelegate = CheckLength;
+        Predicate<string> checkLengthDelegate = CheckLength;
         bool status = checkLengthDelegate.Invoke("skill_factory");
         Console.WriteLine(status);
 
