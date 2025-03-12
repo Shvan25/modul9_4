7c62d132b57ca858733d85658ba471cf3102966e
Name <vbrfqlj@mail.ru>
2025-03-12 19:46:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Programm
+{
+    public delegate int MinDelegate(int x, int y);
+    static void Main(string[] args)
+    {
+        MinDelegate minDelegate = Min;
+        Console.WriteLine(minDelegate.Invoke(33, 20));
+        Console.ReadKey();
+    }
+    static int Min(int x, int y)
+    {
+        return x - y;
+    }
+}
